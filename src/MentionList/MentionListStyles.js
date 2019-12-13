3103320fa8cd5477e1a2a777d2bcb14d20a26c75
Ranzel Manalo <rmanalo@getdevs.com>
2019-12-13 23:04:30 +0800

    fix bugs1

--- src/MentionList/MentionListStyles.js
+++ src/MentionList/MentionListStyles.js
@@ -3,7 +3,9 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   container: {
     // flex:1,
-    maxHeight: 300
+    maxHeight: 300,
+    borderTopColor: "#c6d32e",
+    borderTopWidth: 2
   },
   suggestionsPanelStyle: {},
   loaderContainer: {},
