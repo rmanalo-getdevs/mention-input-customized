cd8903fb4eeea3f4f19778753607a1a7829a4898
Ranzel Manalo <rmanalo@getdevs.com>
2020-02-21 23:54:08 +0800

    fixes2

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -599,7 +599,11 @@ export class Editor extends React.Component {
                 numberOfLines={100}
                 name={"message"}
                 value={state.inputText}
-                onBlur={props.toggleEditor}
+                onBlur={() => {
+                  if (state.inputText === "") {
+                    this.setState({ selection: { start: 0, end: 0 } });
+                  }
+                }}
                 onChangeText={this.onChange}
                 selection={this.state.selection}
                 selectionColor={"#000"}
